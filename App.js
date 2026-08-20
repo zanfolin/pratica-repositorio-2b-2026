f87ee4ef3961f4d7474a495a0a0c1b2517b84cef
Graziani Zanfolin <zanfolin@gmail.com>
2026-08-20 11:08:52 -0300

    [feat] Instalação Gluestack

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function App() {
   return (
-    <View style={styles.container}>
+    
+    <GluestackUIProvider mode="dark">
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </GluestackUIProvider>
+  
   );
 }
 
